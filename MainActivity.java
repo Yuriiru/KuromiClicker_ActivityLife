0944a8a1e4b7300a8cd0dffdee011ad41e27eaad
cemetryghost <96484941+cemetryghost@users.noreply.github.com>
2022-03-06 14:43:07 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,18 +2,93 @@ package com.example.myclicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Toast;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
     private Integer counter = 0;
+    public static final String TAG = "StartActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView counterView = findViewById(R.id.txt_counter);
+        if (savedInstanceState != null && savedInstanceState.containsKey("counter"))
+        {
+            counter = savedInstanceState.getInt("counter");
+        }
+        counterView.setText(counter.toString());
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("counter");
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+        resetUI();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    private void resetUI()
+    {
+        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
+        Log.d(TAG, "resetUI");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+        Log.d(TAG, "onSaveInstanceState");
     }
 
     public void onClickBtnAddHearts(View view) {
@@ -41,5 +116,7 @@ public class MainActivity extends AppCompatActivity {
             rectangle.width = 450;
             layout.setLayoutParams(rectangle);
         }
+
     }
+
 }
